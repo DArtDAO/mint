ae6b932198eb98c8123659cb6cd5e2a9372bb1b5
DArt DAO <contact@dartdao.com>
2022-04-25 18:42:51 +0530

    Update _app.tsx

--- packages/minter-admin-frontend/pages/_app.tsx
+++ packages/minter-admin-frontend/pages/_app.tsx
@@ -22,7 +22,7 @@ const theme = extendTheme ({
       // styles for the `body`
       body: {
         bg: '#00c2cb',
-        font: "Courier New", monospace,
+        font-family: "Courier New", monospace,
       },
     },
   },
